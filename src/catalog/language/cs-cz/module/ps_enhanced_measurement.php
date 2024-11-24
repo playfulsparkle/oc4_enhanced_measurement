94e1425997d0df7a551eb00bce1e769da77fdcf2
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2024-11-24 12:35:24 +0100

    lang

---
+++ src/catalog/language/cs-cz/module/ps_enhanced_measurement.php
@@ -0,0 +1,14 @@
+<?php
+// Text
+$_['text_x_products']            = '%s produkty';
+$_['text_x_related_products']    = '%s související produkty';
+$_['text_purchased_products']    = 'Zakoupené produkty';
+$_['text_checkout_products']     = 'Produkty v košíku';
+$_['text_cart_products']         = 'Produkty v nákupním košíku';
+$_['text_x_search_products']     = '%s - výsledky hledání';
+$_['text_x_tag_products']        = '%s - Produkty s tagem';
+$_['text_search_products']       = 'Hledat produkty';
+$_['text_special_products']      = 'Akční produkty';
+$_['text_compare_products']      = 'Produkty ke srovnání';
+$_['text_manufacturer_products'] = 'Produkty výrobce';
+$_['text_wishlist_products']     = 'Produkty v seznamu přání';
---
+++ src/catalog/language/de-de/module/ps_enhanced_measurement.php
@@ -0,0 +1,14 @@
+<?php
+// Text
+$_['text_x_products']            = '%s Produkte';
+$_['text_x_related_products']    = '%s verwandte Produkte';
+$_['text_purchased_products']    = 'Gekaufte Produkte';
+$_['text_checkout_products']     = 'Produkte im Checkout';
+$_['text_cart_products']         = 'Produkte im Warenkorb';
+$_['text_x_search_products']     = '%s - Suchergebnisse';
+$_['text_x_tag_products']        = '%s - mit Stichwörtern versehene Produkte';
+$_['text_search_products']       = 'Produkte suchen';
+$_['text_special_products']      = 'Promotionsprodukte';
+$_['text_compare_products']      = 'Vergleichsprodukte';
+$_['text_manufacturer_products'] = 'Herstellerprodukte';
+$_['text_wishlist_products']     = 'Wunschlistenprodukte';
